e42a22b15a263c78336ff42d2531ba18a6afb0db
Nathan Cobbald <77047843+Real4XoR@users.noreply.github.com>
2023-08-05 18:03:59 +1200

    Delete main directory

--- main/Coord.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-record Coord(int x, int y) {
-  public Point toPoint() {
-    return new Point(x + 0.5d, y + 0.5d);
-  }
-}
--- main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import javax.swing.SwingUtilities;
-
-public class Main {
-  public static void main(String[]a){
-    SwingUtilities.invokeLater(Compact::new);
-  }
-}
